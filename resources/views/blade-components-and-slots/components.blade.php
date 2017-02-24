e336f8d2ec5effb9d650a1a36001bccc70feeadc
ivan <deselbi@gmail.com>
2017-02-24 18:28:20 +0100

    Whats new in 5.4 - Blade Components and Slots
    page updated to use components and slots

--- resources/views/blade-components-and-slots/components.blade.php
+++ resources/views/blade-components-and-slots/components.blade.php
@@ -8,14 +8,29 @@
             <div class="columns">
 
                 <div class="col-md-4">
-                    @include('partials.panel', [
-                        'header' => 'Header Message'
-                    ])
+                    @component('partials.panel')
+                        @slot('header')
+                            Header Message
+                        @endslot
+
+                        @slot('body')
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+                        @endslot
+                    @endcomponent
                 </div>
+
                 <div class="col-md-4">
-                    @include('partials.panel', [
-                        'header' => ' Different Header Message'
-                    ])
+                    @component('partials.panel')
+                        @slot('header')
+                            Different Header Message
+                        @endslot
+
+                        @slot('body')
+                            different body
+                        @endslot
+
+                         default slot text
+                    @endcomponent
                 </div>
 
             </div>
--- tests/whatsnewin5.4/BladeComponentsAndSlotsTest.php
+++ tests/whatsnewin5.4/BladeComponentsAndSlotsTest.php
@@ -20,6 +20,9 @@ class BladeComponentsAndSlotsTest extends TestCase
         $response->assertSee("Header Message");
         $response->assertSee("Different Header Message");
         $response->assertSee("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum");
+        $response->assertSee("default slot text");
     }
+
+
 
 }
